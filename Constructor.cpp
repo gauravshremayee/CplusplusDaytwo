77cefe132cadfcdb7d635a63560d0419cbcfe573
Gaurav <gaurav.2897@gmail.com>
2019-09-03 22:51:53 +0530

    Create Constructor.cpp

---
+++ Constructor.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Test{
+
+        public:
+                int a,b;
+
+                Test(){
+                        a=2;
+                }
+
+};
+
+
+int main(){
+
+        Test t;
+
+        cout<<t.a;
+
+        return 0;
+
+}
